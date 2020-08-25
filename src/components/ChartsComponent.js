4a8d3be45ac89d5c62e4bc7430c0955d72a8eceb
ahsanshah <shahahsan56@gmail.com>
2020-08-25 18:03:23 +0500

    Added Charts

--- src/components/ChartsComponent.js
+++ src/components/ChartsComponent.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {fetchDailyData} from '../api';
+import {Line,Bar} from 'react-chartjs-2';
 
+import styles from './Charts.module.css';
 
 const Charts = () => {
+    
+    const[dailyData,setDailyData]=useState([]);
+
+    const[isFetched,setFetched]=useState(false);
+
+    useEffect(()=>{
+        const fetchAPI=async()=>{
+            const fetchedData=await fetchDailyData();
+            setDailyData(fetchedData);
+        }
+
+        console.log(dailyData);
+      
+            fetchAPI();
+            setFetched(true);
+        }
+    ,[dailyData, isFetched]);
+
+    const LineChart=(
+        dailyData.length?
+        <Line 
+            data={{
+                labels:dailyData.map(({date})=>date),
+                datasets:[{
+                    data:dailyData.map(({confirmed})=>confirmed),
+                    label:'Infected',
+                    borderColor:'#3333ff',
+                    fill:true
+                },{
+                    data:dailyData.map(({deaths})=>deaths),
+                    label:'Deaths',
+                    borderColor:'red',
+                    backgrounColor:'rgba(255,0,0,0.5)',
+                    fill:true
+                }]
+            }}
+            />:null
+        );
+
+
+
     return ( 
-        <div>
-            <h1>Chart</h1>
+        <div className={styles.container}>
+            {LineChart}
         </div>
      );
 }
